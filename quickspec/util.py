16615b861cb2d03148866470e0ff5399c75a2dc9
Duncan Hanson <duncan.hanson@gmail.com>
2014-06-30 16:13:09 -0400

    add numerical differentiation function using 3-point lagrange interpolation

--- quickspec/util.py
+++ quickspec/util.py
@@ -18,3 +18,34 @@ def enumerate_progress(list, label=''):
                               label + " " + int(10. * cpct / 100)*"-" + "> " + ("%02d" % cpct) + r"%" )
             sys.stdout.flush()
     sys.stdout.write("\n"); sys.stdout.flush()
+
+def deriv(xv, yv=None):
+    """ numerical differentiation based on 3-point lagrange interpolation, following the IDL 'DERIV' function. """
+    ni = len(xv)
+    assert(ni >= 3)
+
+    if yv == None:
+        yv = xv
+        xv = np.arange(0, ni)
+    assert( len(yv) == len(xv) )
+
+    idxs = {}
+    for i in xrange(-1, 2):
+        idxs[i] = np.arange(0, ni) + i
+        idxs[i][0:1] = i+1
+        idxs[i][ni-1:ni] = i+ni-2
+
+    ret = np.zeros(ni)
+    for i in xrange(-1, 2):
+        num = np.zeros(ni)
+        den = np.ones(ni)
+
+        for j in xrange(-1, 2):
+            if i==j: continue
+
+            num += (xv - xv[idxs[j]])
+            den *= (xv[idxs[i]] - xv[idxs[j]])
+
+        ret += yv[idxs[i]] * num / den
+
+    return ret
